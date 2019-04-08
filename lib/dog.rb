ab5664b306bb1c5a87ca1a9d513c9c2c7130400a
Daniel Alvarez <dnielalvrz@gmail.com>
2019-04-08 20:48:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,18 @@
 class Dog
-  def name(name)
-    @dog_name = name
-  end
 
   def name=(name)
-    @dog_name
+    @name = name
+  end
+
+  def name
+    "#{@name}"
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    "#{@breed}"
+  end
 end
